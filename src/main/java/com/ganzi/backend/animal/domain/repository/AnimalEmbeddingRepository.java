a186c69a6aff552a52371edcbade204a9bfa27f3
SXXHU <dofqls0127@gmail.com>
2025-11-15 14:54:12 +0900

    fix/#24: AnimalEmbeddingRepository key 타입 변경 Long -> String

--- src/main/java/com/ganzi/backend/animal/domain/repository/AnimalEmbeddingRepository.java
+++ src/main/java/com/ganzi/backend/animal/domain/repository/AnimalEmbeddingRepository.java
@@ -3,5 +3,5 @@ package com.ganzi.backend.animal.domain.repository;
 import com.ganzi.backend.animal.domain.AnimalEmbedding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalEmbeddingRepository extends JpaRepository<AnimalEmbedding, Long> {
+public interface AnimalEmbeddingRepository extends JpaRepository<AnimalEmbedding, String> {
 }
