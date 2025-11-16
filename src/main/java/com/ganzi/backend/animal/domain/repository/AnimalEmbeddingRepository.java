4af30c436a9be5c12638e7a6fe9598268751b432
SXXHU <dofqls0127@gmail.com>
2025-11-16 21:18:12 +0900

    refactor/#31: findByDesertionNo 커스텀 메서드 삭제

--- src/main/java/com/ganzi/backend/animal/domain/repository/AnimalEmbeddingRepository.java
+++ src/main/java/com/ganzi/backend/animal/domain/repository/AnimalEmbeddingRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AnimalEmbeddingRepository extends JpaRepository<AnimalEmbedding, String> {
-    Optional<AnimalEmbedding> findByDesertionNo(String desertionNo);
 }
