9bd81023d7c9d686c9baa579a25df81b3a452f30
SXXHU <dofqls0127@gmail.com>
2025-11-16 21:16:44 +0900

    refactor/#31: 관리자용 user interest record API로 명시화 및 `@PreAuthorize` 추가

--- src/main/java/com/ganzi/backend/user/api/UserInterestController.java
+++ src/main/java/com/ganzi/backend/user/api/UserInterestController.java
@@ -1,32 +1,36 @@
 package com.ganzi.backend.user.api;
 
 import com.ganzi.backend.global.code.dto.ApiResponse;
-import com.ganzi.backend.user.UserInterestService;
+import com.ganzi.backend.user.application.UserInterestService;
 import com.ganzi.backend.user.api.doc.UserInterestControllerDoc;
 import com.ganzi.backend.user.api.dto.RecordInterestRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user-interests")
+@RequestMapping("/api/admin/user-interests")
 public class UserInterestController implements UserInterestControllerDoc {
 
     private final UserInterestService userInterestService;
 
+    @Override
     @PostMapping("/{userId}/interests")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<String>> recordUserInterest(
-        @PathVariable("userId") Long userId,
-        @Valid @RequestBody RecordInterestRequest request) {
+            @PathVariable("userId") Long userId,
+            @Valid @RequestBody RecordInterestRequest request) {
 
         userInterestService.recordInterest(
                 userId,
-                request.getDesertionNo(),
-                request.getDwellTimeSeconds(),
-                request.isLiked()
+                request.desertionNo(),
+                request.dwellTimeSeconds(),
+                request.liked()
         );
-    return ResponseEntity.ok(ApiResponse.onSuccess("유저의 행동 데이터를 기록하였습니다"));
+
+        return ResponseEntity.ok(ApiResponse.onSuccess("유저의 행동 데이터를 기록하였습니다"));
     }
 }
