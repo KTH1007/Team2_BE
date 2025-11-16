4b04969148efb4f1f8ceccbce45b8ec06512bc6a
SXXHU <dofqls0127@gmail.com>
2025-11-16 21:16:57 +0900

    refactor/#31: RecordInterestRequest를 record로 전환

--- src/main/java/com/ganzi/backend/user/api/dto/RecordInterestRequest.java
+++ src/main/java/com/ganzi/backend/user/api/dto/RecordInterestRequest.java
@@ -1,14 +1,9 @@
 package com.ganzi.backend.user.api.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
-@Getter @Setter
-@NoArgsConstructor
-public class RecordInterestRequest {
-
-    private String desertionNo;
-    private Integer dwellTimeSeconds;
-    private boolean liked;
-}
+public record RecordInterestRequest (
+        @NotBlank String desertionNo,
+        Integer dwellTimeSeconds,
+        boolean liked
+){}
